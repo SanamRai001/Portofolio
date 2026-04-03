17d7c487fbe6bc697c4c291a3949385f2bdb2369
Sanam Rai <sanamr571@outlook.com>
2026-04-03 20:52:43 +0545

    next task cache flow

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,13 +1,44 @@
 import mongoose from "mongoose";
 import Project from "../models/projectModel.js";
+import NodeCache from "node-cache";
+import System from "../models/systemModel.js";
+
+// take systemconfig from database
+// check database toggle is true or false
+// if  true
+// then check cache if true
+// check  cache if  yes return cachce
+// else fetch from db 
+// if cache then store in cache
+// return response
+
+const systemConfig = await System.findOne({});
+if(systemConfig?.db){
+    if(systemConfig?.cache){
+        const projectCache = new NodeCache({stdTTL:100, checkperiod:120});
+        if(projectCache.has(key)){
+            return json({
+                success: true,
+                message: "Projects fetched successfully from Cache",
+                data: value
+            });
+        }
+        else{
+            const projects =  await Project.find();
+            const value  =  projectCache.get("projects");
+        }
+    }
+}
+
 
 export const getProjects = async (req, res)=>{
     try{
+       
         if(req.systemConfig?.db === true){
             const projects =  await Project.find();
             res.json({
                 success: true,
-                message: "Projects  fetched successfully",
+                message: "Projects  fetched successfully From DB",
                 data: projects
             });
         }
