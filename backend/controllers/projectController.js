77d14857e40241304b921b074655f1b69c469acc
Sanam Rai <sanamr571@outlook.com>
2026-04-04 21:26:27 +0545

    added pagination logic in backend

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -5,8 +5,20 @@ import System from "../models/systemModel.js";
 
 const projectCache = new NodeCache({stdTTL:100, checkperiod:120});
 
+const pagination = (projects, pageNumber, limit) =>{
+    const start = (pageNumber-1)*limit;
+    const end = start + limit;
+    return projects.slice(start, end); 
+}
 
 export const getProjects = async (req, res)=>{
+    const pageNumber = req.systemConfig?.pagination
+  ? parseInt(req.query.pageNumber) || 1
+  : 1;
+
+const limit = req.systemConfig?.pagination
+  ? parseInt(req.query.limit) || 3
+  : 3;
     if(req.systemConfig?.db === false){
         return  res.json({
                 success: false,
@@ -17,18 +29,25 @@ export const getProjects = async (req, res)=>{
     if(req.systemConfig?.cache ===true){
         if(projectCache.has("projects")){
             console.log("Fetching  data  from cache");
+            let cacheProject = projectCache.get("projects");
+            if(req.systemConfig?.pagination === true){
+                cacheProject = pagination(cacheProject, pageNumber, limit);
+            }
             return res.json({
                 success: true,
                 message: "Projects fetched successfully from Cache",
-                data: projectCache.get("projects")
+                data: cacheProject
             });
         }
     }
     try{
-        const projects =  await Project.find();
+        let projects =  await Project.find();
         if(req.systemConfig?.cache === true){
             projectCache.set("projects", projects);
             console.log("DB HIT");
+        }
+        if(req.systemConfig?.pagination === true){
+            projects = pagination(projects, pageNumber,limit);
         }
          res.json({
             success: true,
