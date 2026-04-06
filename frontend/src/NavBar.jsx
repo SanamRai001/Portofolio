df525d73bc0ada76e3459af36fdb4c90793942a7
Sanam Rai <sanamr571@outlook.com>
2026-04-06 16:42:43 +0545

    added logo

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-
+import logo  from './assets/logo.png'
 const NavBar = () => {
   return (
     <>
         <nav className='Nav'>
-            <a href="" className='NavItem'>Logo Image</a>
+            <a href="" className='NavItem'> <img className= 'img' src={logo} alt="my logo"  /> </a>
             <ul>
                 <li><a href="" className='NavItem'>System Controls</a></li>
                 <li><a href="" className='NavItem'>Projects</a></li>
                 <li><a href="" className='NavItem'>Resume</a></li>
-                <button>Fetch Data</button>  
             </ul>
         </nav>
     </>
